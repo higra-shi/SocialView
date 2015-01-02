4b0fcbb46d26c94ed414dff8e1d61ddb8f02c9d3
Shusaku HARADA <cawasemi@gmail.com>
2015-01-02 17:39:12 +0900

    Create Project

---
+++ SocialView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialView
+//
+//  Created by 原田 周作 on 2015/01/02.
+//  Copyright (c) 2015年 Shusaku HARADA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
